b158e3237532ac2eb9d046a30a5d6b04711b9cc8
Desmond.Gan <desmond.gan@health.telstra.com>
2025-04-12 12:30:47 +1000

    Remove not needed virtual on base class model property

--- RetentionReleaseManager/Models/Abstract/BaseModel.cs
+++ RetentionReleaseManager/Models/Abstract/BaseModel.cs
@@ -2,7 +2,7 @@ namespace RetentionReleaseManager.Models.Abstract;
 
 public abstract class BaseModel
 {
-    public virtual string Id { get; set; }
+    public string Id { get; set; }
     
     public BaseModel(string id)
     {
